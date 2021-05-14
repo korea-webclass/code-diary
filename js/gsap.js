85715327eb1d52fd4144a71d55b9915efa903d8e
webclass <korea.webclass@gmail.com>
2021-05-14 23:42:20 +0900

    feat : gsap 공부 3일차
    feat : 복습 및 정리 내용 추가

--- js/gsap.js
+++ js/gsap.js
@@ -4,31 +4,156 @@
     최소한 display: inline-block 로 해야한다.
 */
 
+$(function () {
 
-$(function (){
-
-    // function top_bar() {
-    //     var $menu1ItemLine = $('.top-bar__menu-1-item-line');
-    //     $('.top-bar__menu-1 > ul > li').mouseenter(function() {
-    //         var $li = $(this);
-
-    //         var left = $li.position().left;
-    //         var width = $li.outerWidth();
-
-    //         $menu1ItemLine.css({
-    //             left: left,
-    //             width: width
-    //         });
-    //         // console.log("left : " + left + ", width: " + width);
-    //     });
-    //     $('.top-bar__menu-1 > ul > li').eq(0).mouseenter();
-    // };
-    // top_bar();
+    
 
 
 
 
 
+
+
+
+
+
+    // 4편
+    // gsap.timeline()
+    // .to('.box', {
+    //     duration: 1, 
+    //     rotation: 360, 
+    //     y: 100,
+    //     stagger: {
+    //         amount: 1,
+    //         from: 'center',
+    //     } 
+    // })
+    // .to('.box', {
+    //     duration: 1, 
+    //     rotation: 0, 
+    //     y: 0,
+    //     stagger: {
+    //         amount: 1,
+    //         from: 'center',
+    //     } 
+    // });
+
+    // gsap.to('.demo > .box', {
+    //     y: 100,
+    //     duration: 1,
+    //     stagger: {
+    //         // each: 1,
+    //         amount: 1,
+    //         from: 'edges',
+    //     },
+    // });
+
+    // gsap.to('.demo > .box', {
+    //     y: 100,
+    //     duration: 1,
+    //     stagger: 0.3,
+    // });
+
+    // 순서대로 애니메이션 주는 코드
+    // var tl1 = gsap.timeline();
+    // tl1.to('.demo > .box:nth-child(1)', {
+    //     y: 100,
+    //     duration: 1,
+    // });
+    // tl1.to('.demo > .box:nth-child(2)', {
+    //     y: 100,
+    //     duration: 1,
+    // }, '-=0.9');
+    // tl1.to('.demo > .box:nth-child(3)', {
+    //     y: 100,
+    //     duration: 1,
+    // }, '-=0.9');
+    // tl1.to('.demo > .box:nth-child(4)', {
+    //     y: 100,
+    //     duration: 1,
+    // }, '-=0.9');
+    // tl1.to('.demo > .box:nth-child(5)', {
+    //     y: 100,
+    //     duration: 1,
+    // }, '-=0.9');
+
+
+
+
+
+
+
+
+
+    // 3편
+    // gsap.fromTo('.box-1', {x: 100}, {
+    //     y: 500, 
+    //     duration: 3,
+    // });
+
+    // var tl1 = gsap.timeline();
+    // tl1.fromTo('.box-1', {x:0}, {
+    //     duration: 2,
+    //     x: 500,
+    // });
+    // tl1.to('.box-1', {
+    //     duration: 2,
+    //     x: 100,
+    // });
+
+
+
+
+
+
+
+
+
+    // 2편
+    // timeline(); 및 순차적으로 애니메이션
+    // var tl1 = gsap.timeline();
+
+    // tl1.to('.box-1', {
+    //     x: 500,
+    //     // x: -500,
+    //     duration: 3,
+    // })
+    // .to('.box-1', {
+    //     rotation: 360,
+    //     duration: 5,
+    // }, '-=3')
+    // .to('.box-1', {
+    //     backgroundColor: 'pink',
+    //     duration: 1,
+    // })
+    // .to('.box-1', {
+    //     backgroundColor: 'red',
+    //     x: 0,
+    //     duration: 10,
+    // });
+
+
+
+
+
+
+
+
+
+
+    // 1편
+    // 기본 밑으로 내려가는 애니메이션
+    // gsap.to('.box-1', {
+    //     x: 500, // 가로값
+    //     // rotation: 45, // 회전하기
+    //     duration: 5 // 시간
+    // });
+
+    // gsap.from('.box-2', {
+    //     x: 500, // 가로값
+    //     // rotation: 45, // 회전하기
+    //     duration: 5 // 시간
+    // });
 
 
 
@@ -40,131 +165,25 @@ $(function (){
 
 
     // 스크롤 트리거 플러그인 활성화
-    gsap.registerPlugin(ScrollTrigger);
-
-    gsap.from('.section-1 > img', {
-        duration: 1,
-        x: 400,
-        opacity: 0,
-        scrollTrigger: {
-            trigger: '.section-1 > img', 
-            markers: true, 
-            start: "top 50%", 
-            end: 'bottom 20%', 
-            toggleActions: 'play none none reverse', 
-        }
-    });
-
-    gsap.from('.section-2 > img', {
-        duration: 1,
-        y: 400,
-        opacity: 0,
-        scrollTrigger: '.section-2 > img',
-    });
-
-
-
-
-
-
     // gsap.registerPlugin(ScrollTrigger);
 
-    // gsap.to('.box-01', {
+    // gsap.from('.section-1 > img', {
+    //     duration: 1,
     //     x: 400,
-    //     rotation: 360,
-    //     duration: 4,
-    // });
-
-    // gsap.to('.box-02', {
+    //     opacity: 0,
     //     scrollTrigger: {
-    //         trigger: '.box-05',
-    //         markers: true,
-    //         start: "center 50%",
-    //         end: 'bottom, 10%',
-    //         toggleActions: 'play pause reverse none',
-    //     }, 
-    //     x: 400,
-    //     rotation: 360,
-    //     duration: 4,
+    //         trigger: '.section-1 > img', 
+    //         markers: true, 
+    //         start: 'top 50%', 
+    //         end: 'bottom 20%', 
+    //         toggleActions: 'play none none reverse', 
+    //     }
     // });
 
-
-
-    // var t1 = gsap.timeline({
-    //     repeat: -1,
-    //     repeatDelay: 1,
-    // });
-
-    // t1.from('.box', {
-    //     y: 200, 
-    //     duration: 1, 
-    //     stagger: 0.2
-    // });
-    // t1.to('.box', {
-    //     y: -200, 
-    //     duration: 1, 
-    //     stagger: 0.2
-    // });
-
-
-
-    // var tl3 = gsap.timeline();
-    // tl3.to('.box', {x: 100, duration: 1, stagger: {
-    //     each: 0.5,
-    //     amount: 1,
-    //     from: 'end',
-    //     from: 'edges',
-    //     from: 'center',
-    // }});
-
-
-
-    // var tl3 = gsap.timeline();
-    // tl3.to('.box-01', {x: 100, duration: 1})
-    // tl3.to('.box-02', {x: 100, duration: 1}, '-=0.8')
-    // tl3.to('.box-03', {x: 100, duration: 1}, '-=0.8')
-    // tl3.to('.box-04', {x: 100, duration: 1}, '-=0.8')
-    // tl3.to('.box-05', {x: 100, duration: 1}, '-=0.8')
-    // tl3.to('.box-06', {x: 100, duration: 1}, '-=0.8')
-    // tl3.to('.box-07', {x: 100, duration: 1}, '-=0.8')
-    // tl3.to('.box-08', {x: 100, duration: 1}, '-=0.8')
-    // tl3.to('.box-09', {x: 100, duration: 1}, '-=0.8')
-
-
-
-    // var tl2 = gsap.timeline();
-    // tl2.fromTo('.box-01', 
-    //     {y: 50}, 
-    //     {duration: 3, x: 500, y: 0},
-    // );
-    // tl2.to('.box-01', 
-    //     {duration: 3, x: 0, y: 0},
-    // );
-    
-
-
-    // gsap.fromTo('.box-01', {
-    //     y: 200
-    // },{
-    //     duration: 3,
-    //     x: 500,
-    //     y: 0,
-    // });
-
-
-
-    // var tl1 = gsap.timeline();
-    // tl1.to('.box-01', {
-    //     duration: 3, 
-    //     x: 500,
-    // }).to('.box-01', {
-    //     duration: 3,
-    //     backgroundColor: 'pink',
-    // });
-
-    // // 기본 밑으로 내려가는 애니메이션
-    // gsap.to('.box-02', {
-    //     y: 200,
-    //     duration: 1.5,
+    // gsap.from('.section-2 > img', {
+    //     duration: 1,
+    //     y: 400,
+    //     opacity: 0,
+    //     scrollTrigger: '.section-2 > img',
     // });
 });
